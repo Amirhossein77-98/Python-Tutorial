1b7cb4d6eae99b804adc40082d6e6435437d4fed
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2024-04-27 11:30:55 +0330

    Organized Everything

---
+++ English/variables_en.py
@@ -0,0 +1,21 @@
+# Variables definition
+name = "Amirhossein"
+age = 55
+is_young = True
+something = 15.62345
+some_list = []
+some_tuple = ()
+
+# Any Limitation?
+name, age, is_yong = "Amirhossein", 55, True
+
+print(name)
+
+# Variable naming conventions
+
+# Modify and change variables
+name = "Ali"
+
+print(name)
+
+print(age + something)
---
+++ Persian/variables_fa.py
@@ -0,0 +1,20 @@
+# تعریف متغیر
+name = "Amirhossein"
+age = 45
+is_yong = True
+something = 15.2649
+some_list = ["Amir", 25]
+
+# محدودیت؟
+
+# سنت های نام گذاری متغیر ها
+
+# تغییر متغیر ها
+
+print(name)
+
+name = "Ali"
+
+print(name)
+
+print(age + something)
